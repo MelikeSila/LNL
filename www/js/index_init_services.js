059b3339e74b4bf19d9869ecfb04234b6b8c8a40
MelikeSila <melike-sila-keskin@hotmail.com>
2015-09-03 10:24:57 +0300

    logout kısmı araştırılıyor

---
+++ www/js/index_init_services.js
@@ -0,0 +1,8 @@
+
+data_support.ready(intel.xdk.services.LNLAyatar3Bolge.bind(null, {"xdkFilter":null}));
+data_support.ready(intel.xdk.services.DevicesData.bind(null, {"xdkFilter":null}));
+data_support.ready(intel.xdk.services.LNLAyatar3Dynamic.bind(null, {"xdkFilter":null}));
+data_support.ready(intel.xdk.services.LNLVirtual1.bind(null, {"xdkFilter":null}));
+data_support.ready(intel.xdk.services.LNLAyatarData1.bind(null, {"xdkFilter":null}));
+data_support.ready(intel.xdk.services.LNLScenes.bind(null, {"xdkFilter":null}));
+data_support.ready(intel.xdk.services.LNLonscenedevices.bind(null, {"xdkFilter":null}));
---
+++ www/js/index_user_scripts.js
@@ -0,0 +1,75 @@
+(function()
+{
+ "use strict";
+ /*
+   hook up event handlers 
+ */
+ function register_event_handlers()
+ {
+        
+        /* button  Akıllı Görevler */
+    $(document).on("click", ".uib_w_16", function(evt)
+    {
+        /* your code goes here */ 
+    });
+        
+        /* button  {{_name}} */
+    $(document).on("click", ".uib_w_18", function(evt)
+    {
+         activate_subpage("#uib_page_5"); 
+    });
+    
+        /* button  Geri */
+    $(document).on("click", ".uib_w_24", function(evt)
+    {
+         activate_subpage("#uib_page_3"); 
+    });
+    
+        /* button  Geri */
+    $(document).on("click", ".uib_w_25", function(evt)
+    {
+         activate_subpage("#uib_page_4"); 
+    });
+    
+        /* button  Anasayfa */
+    $(document).on("click", ".uib_w_39", function(evt)
+    {
+         activate_subpage("#uib_page_3"); 
+    });
+    
+        /* button  Güvenli Çıkış */
+    
+        
+        /* button  {{_name}} */
+    $(document).on("click", ".uib_w_8", function(evt)
+    {
+         activate_subpage("#uib_page_4"); 
+    });
+     
+        /* listitem  .uib_w_45 */
+    $(document).on("click", ".uib_w_45", function(evt)
+    {
+         activate_subpage("#uib_page_4"); 
+    });
+    
+        /* button  Location */
+    $(document).on("click", ".uib_w_48", function(evt)
+    {
+         activate_subpage("#uib_page_5"); 
+    });
+    
+        /* listitem  {{_name}} */
+    $(document).on("click", ".uib_w_56", function(evt)
+    {
+         activate_subpage("#uib_page_4"); 
+    });
+    
+        /* button  Güvenli Çıkış */
+    $(document).on("click", ".uib_w_17", function(evt)
+    {
+         activate_subpage("#uib_page_7"); 
+    });
+    
+    }
+ document.addEventListener("app.Ready", register_event_handlers, false);
+})();
